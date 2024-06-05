98087e1a9e4716495dda7c3eee86cbd19113d50a
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-06-05 14:50:58 +0500

    login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,35 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage
+    <div className='p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center'>
+      {/* BOX */}
+      <div className='h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[80%] lg:w-[60%] 2xl:w-1/2'>
 
+        {/* image container */}
+        <div className='relative h-1/3 w-full md:h-full md:w-1/2 '>
+          <Image src='/loginBg.png' alt='' fill className='object-cover'/>
+        </div>
+        {/* form container */}
+        <div className='p-10 flex flex-col gap-8 '>
+          <h1 className='font-bold text-xl '>Welcome</h1>
+          <p>Login into your account or create a new one using social buttons</p>
+          <button className='flex gap-4 p-4 ring-1 ring-orange-100 rounded-md'>
+            <Image src='/google.png' alt='' width={20} height={20} className='object-contain' />
+            <span>Sign in with Goggle </span>
+          </button>
+          <button className='flex gap-4 p-4 ring-1 ring-orange-100 rounded-md'>
+            <Image src='/facebook.png' alt='' width={20} height={20} className='object-contain' />
+            <span>Sign in with Facebook </span>
+          </button>
+          <p className='text-sm '>Have a problem?
+            <Link href='/' className='underline px-2'>Contact us </Link>
+          </p>
+        </div>
+
+      </div>
     </div>
   )
 }
