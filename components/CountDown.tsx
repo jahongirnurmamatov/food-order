4a007e4a6c40ee37a7f0fa0c1dfa1a8a53acd45e
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-06-05 09:17:05 +0500

    Offer, featured products, slider part is done

---
+++ components/CountDown.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+import Countdown from 'react-countdown'
+
+const endingDate = new Date('2024-06-07')
+const CountDown = () => {
+  return (
+    <div><Countdown className='font-bold text-5xl text-yellow-300' date={endingDate}/></div>
+  )
+}
+
+export default CountDown
--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,19 +1,46 @@
+"use client"
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+const data = [
+    {
+      id: 1,
+      title: "always fresh & always crispy & always hot",
+      image: "/slide1.png",
+    },
+    {
+      id: 2,
+      title: "we deliver your order wherever you are in NY",
+      image: "/slide2.png",
+    },
+    {
+      id: 3,
+      title: "the best pizza to share with your family",
+      image: "/slide3.jpg",
+    },
+  ];
 
 const Slider = () => {
+    const [currenSlide, setCurrentSlide] = useState(0);
+    useEffect(()=>{
+        const interval = setInterval(
+            ()=>setCurrentSlide(prev=>prev=== data.length-1 ? 0: prev+1)
+    ,2000);
+        return ()=> clearInterval(interval);
+
+    },[])
   return (
-    <div className='flex flex-col  h-screen '>
+    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]  lg:flex-row'>
         {/* TEXT CONTAINER */}
-        <div className='h-1/2'>
-            <h1>Test</h1>
-            <button>Order now</button>
+        <div className='h-1/2 flex items-center justify-center flex-col gap-8 lg:w-1/2 bg-fuchsia-50 text-red-500 font-bold lg:h-full'>
+            <h1 className='text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl'>{data[currenSlide].title}</h1>
+            <button className='bg-red-500 text-white py-4 px-8'>Order now</button>
         </div>
         {/* IMAGE CONTAINER */}
-        <div className='h-1/2'> 
-            <Image src='/slide1.png' alt='' fill className='w-full'/>
+        <div className='h-1/2  w-full relative lg:h-full lg:w-1/2' > 
+            <Image src={data[currenSlide].image} alt='' fill className='object-cover'/>
         </div>
-
 
     </div>
   )
